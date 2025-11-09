17fc7ab9c7c68d51bd0c8688ff95f47d80033ff7
HA-Kodes <2BianconiHa@gmail.com>
2025-11-09 11:44:44 -0800

    Squashed commit of the following:
    commit 5e12847f289dd796fa19220b19f62e0e2642226c
    Author: HA-Kodes <2BianconiHa@gmail.com>
    Date:   Sun Nov 9 11:34:08 2025 -0800
    
        create UserController.java class

--- src/main/java/com/coderscampus/service/UserService.java
+++ src/main/java/com/coderscampus/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +16,12 @@ public class UserService {
 
     public List<User> findAll () {
         return userRepo.findAll();
+    }
+
+    public User findById(Long userId) {
+        Optional<User> userOpt = userRepo.findById(userId);
+        return userOpt.orElse(new User());
+
     }
 
 }
--- src/main/java/com/coderscampus/web/UserController.java
+++ src/main/java/com/coderscampus/web/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -19,6 +21,13 @@ public class UserController {
     public String getAllUsers (ModelMap model) {
         List<User> users = userService.findAll();
         model.put("users", users);
+        return "users";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String getOneUser (ModelMap model, @PathVariable Long userId) {
+        userService.findOne(userId);
+        model.put("users", Arrays.asList(user));
         return "users";
     }
 }
