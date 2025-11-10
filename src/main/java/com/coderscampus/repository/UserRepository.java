088ebaaef97ff369058c6ea5709f783d7a4410b5
HA-Kodes <2BianconiHa@gmail.com>
2025-11-09 22:00:32 -0800

    JPQL Custom Queries

--- src/main/java/com/coderscampus/repository/UserRepository.java
+++ src/main/java/com/coderscampus/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.coderscampus.repository;
 
 import com.coderscampus.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -21,5 +22,8 @@ public interface UserRepository extends JpaRepository <User, Long> {
 
     // select * from users where name = :name
     List<User> findByCreatedDateBetween(LocalDate date1, LocalDate date2);
+
+    @Query("select u from User u where username = :username")
+    List<User> findExactlyOneUserByUsername(String username);
 
 }
